993ee26e55f729534916c583f7e015d451228d66
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2023-11-27 20:08:05 +0100

    Change setup and add util
    add encryptMessage and decryptMessage to encrpyt and decrypt messages
    given to the functions using the crypto library

---
+++ src/util/crypt.ts
@@ -0,0 +1,27 @@
+import * as crypto from "crypto"
+
+export const encryptMessage = (message: string, password: string) => {
+
+    const iv = crypto.randomBytes(16)
+
+    // const secretKey = password === "" ? crypto.randomBytes(32) : password
+
+    const cipher = crypto.createCipheriv("aes-256-ccm", password, iv)
+
+    let encryptedMessage = cipher.update(message, "utf-8", "hex")
+
+    encryptedMessage += cipher.final("hex")
+
+    return { encryptedMessage, iv }
+}
+
+export const decryptMessage = (message: string, secretKey: string, iv: string) => {
+
+    const decipher = crypto.createDecipheriv("aes-256-ccm", secretKey, Buffer.from(iv))
+
+    let decryptedMessage = decipher.update(message, "hex", "utf-8")
+
+    decryptedMessage += decipher.final("utf-8")
+
+    return decryptedMessage
+}
